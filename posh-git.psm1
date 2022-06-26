5a178d1cf5ce0664daef5f35a0f696267769c787
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2022-06-26 08:24:42 +0530

    Add files via upload

---
+++ posh-git.psm1
@@ -0,0 +1,28 @@
+if (Get-Module posh-git) { return }
+
+Push-Location $psScriptRoot
+.\CheckVersion.ps1 > $null
+
+. .\Utils.ps1
+. .\GitUtils.ps1
+. .\GitPrompt.ps1
+. .\GitTabExpansion.ps1
+. .\TortoiseGit.ps1
+Pop-Location
+
+if (!$Env:HOME) { $Env:HOME = "$Env:HOMEDRIVE$Env:HOMEPATH" }
+if (!$Env:HOME) { $Env:HOME = "$Env:USERPROFILE" }
+
+Export-ModuleMember -Function @(
+        'Write-GitStatus',
+        'Get-GitStatus', 
+        'Enable-GitColors', 
+        'Get-GitDirectory',
+        'TabExpansion',
+        'Get-AliasPattern',
+        'Start-SshAgent',
+        'Stop-SshAgent',
+        'Add-SshKey',
+        'Update-AllBranches',
+        'tgit')
+
